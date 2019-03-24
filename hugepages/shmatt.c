565b7892ad661584ec2a4c1a88d0d7edd25dd08c
ahbaidg <ahbaid@gmail.com>
2019-03-24 16:26:28 -0700

    hugepages

--- hugepages/shmatt.c
+++ hugepages/shmatt.c
@@ -1,6 +1,6 @@
 // Author: Ahbaid Gaffoor
 // Date:   23 April 2008
-// File:   shmget.c
+// File:   shmatt.c
 // Use:    Test Shared Memory Allocation
 
 #include <sys/types.h>
@@ -35,12 +35,23 @@ int main() {
   */
   int shmflags = SHM_HUGETLB|IPC_CREAT|SHM_R|SHM_W;
 
-  /* Allocate a shared memory segment */
+  /* Grab a shared memory segment, will allocate if it does not already exist with the given key */
   int shmid;
   if ((shmid = shmget(shmkey, SHMSZ, shmflags )) == -1) {
      perror("shmget");
   } else {
      printf("shmget returned %d\n",shmid);
+  }
+
+  /* Attach to the segment & loop if found */
+  char *shmseg;
+  if ((shmseg = shmat(shmid, NULL, 0)) == (char *) -1) {
+     perror("shmat");
+  } else {
+     printf("%s\n","Attached.... looping to hold attachment");  
+     while (1) {
+        sleep(1);
+     }
   }
 
   return 0;
